ed31d783f7169a3420d245e571f4d7f104c83a91
Daniel Kogut <danieltugok@gmail.com>
2022-05-19 00:16:47 -0500

    instalacao sqlite, RoomController, imprimindo id sala no layout room.ejs

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,5 +1,45 @@
-module.exports = {
-    create () {
+const Database = require('../db/config');
 
+module.exports = {
+  async create(req, res) {
+    const db = await Database();
+
+    const pass = req.body.password;
+    let roomId;
+
+    let isRoomExists = true;
+    while(isRoomExists) {
+
+      // Gera o numero da sala com 6 digitos
+      for(var i=0; i<6; i++) {
+        i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
+          roomId += Math.floor(Math.random() * 10).toString()
+      }
+
+        // Verifica se a sala ja existe
+      const roomExistsId = await db.all(`SELECT id FROM rooms WHERE id = '${parseInt(roomId)}'`);
+
+      if (roomExistsId.length == 0) {
+        isRoomExists = false;
+
+        // Insere a sala no banco de dados
+        await db.run(`INSERT INTO rooms (
+            id, pass
+          ) VALUES (
+            ${parseInt(roomId)},
+            ${pass}
+          )`);
+      }       
     }
-}
+
+    await db.close();
+
+    res.redirect(`/room/${roomId}`);
+  },
+
+  open( req, res){
+    const roomId = req.params.roomId;
+
+    res.render('room', { roomId });
+  }
+};
--- src/routes.js
+++ src/routes.js
@@ -6,9 +6,10 @@ const route = express.Router();
 
 route.get('/', (req, res) => res.render("index", {page: "enter-room"}));
 route.get('/create-pass', (req, res) => res.render("index", {page: "create-pass"}));
-route.get('/room/:roomId', (req, res) => res.render("room"));
 
-route.post('/room/:room/:question/:action', QuestionController.index);
-route.post('/room/create-room', RoomController.create);
+route.get('/room/:roomId', RoomController.open);
+
+route.post('/question/:room/:question/:action', QuestionController.index);
+route.post('/create-room', RoomController.create);
 
 module.exports = route;
